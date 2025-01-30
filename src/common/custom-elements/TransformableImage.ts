45eefb129937d9dc40f6c36ed196a4766c8572c7
THSlimes <thomkastelein.tk@gmail.com>
2025-01-30 17:03:22 +0100

    TransformableImage now has fit attribute.
    Version is now v0.0.8.

--- src/common/custom-elements/TransformableImage.ts
+++ src/common/custom-elements/TransformableImage.ts
@@ -1,4 +1,5 @@
 import ElementFactory from "../element-factory/ElementFactory";
+import TypeChecker, { getEnumChecker } from "../run-time-type-checking/TypeChecker";
 import Transformable from "./Transformable";
 
 const EF = ElementFactory.INSTANCE;
@@ -18,14 +19,25 @@ class TransformableImage extends Transformable {
         else this.removeAttribute("src");
     }
 
+    public get fit(): TransformableImage.Fit {
+        return TransformableImage.castToFit(this.getAttribute("fit"));
+    }
+
+    public set fit(newVal: TransformableImage.Fit) {
+        this.setAttribute("fit", newVal);
+    }
+
 
     protected override initElement(): void | Promise<void> {
+        if (!this.hasAttribute("fit")) this.fit = TransformableImage.Fit.FILL; // assign default fitting
+
         const imageElement = this.appendChild(
             EF.img({}, this.src ?? TransformableImage.DEFAULT_SRC)
                 .make()
         );
 
-        this.onAttributeChanged("src", newSrc => imageElement.src = newSrc ?? TransformableImage.DEFAULT_SRC);
+        this.onAttributeChanged("src", () => imageElement.src = this.src ?? TransformableImage.DEFAULT_SRC);
+        this.onAttributeChanged("fit", () => imageElement.style.objectFit = this.fit);
 
         super.initElement();
     }
@@ -45,6 +57,8 @@ namespace TransformableImage {
         FILL = "fill",
         SCALE_DOWN = "scale-down"
     }
+
+    export const castToFit = TypeChecker.cast(getEnumChecker(Fit), "TransformableImage.Fit");
 
 }
 
